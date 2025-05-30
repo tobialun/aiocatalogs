865f3041e36540157d244b3bf9a9218892e0ac1e
panteLx <info@pantelx.com>
2025-05-30 02:03:48 +0200

    feat: add Tawk.to script for live chat support in footer component

--- templates/footerComponent.ts
+++ templates/footerComponent.ts
@@ -42,5 +42,17 @@ export function getFooterHTML(packageVersion: string): string {
       </div>
     </div>
   </footer>
+  <script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/6838da7b18e960190e216bec/1isf1asmt';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
   `;
 }
